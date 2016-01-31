8823fb6dde43e9f9ff17307e28907e4c1e7321c0
Konsul117 <konsul117@bk.ru>
2016-01-31 19:11:08 +1000

    Правка инициализации приложения - накат миграций

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=phone_test',
+    'dsn' => 'mysql:host=localhost;dbname=test',
     'username' => 'root',
     'password' => 'qwe',
     'charset' => 'utf8',
